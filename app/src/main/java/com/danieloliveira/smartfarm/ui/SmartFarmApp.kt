702b6b30c87dd676b90ff69c0eaa9d4fa119f8d6
Daniel Oliveira <danielsantosoliveira753@gmail.com>
2022-09-29 14:04:46 -0300

    Initial Setup

---
+++ app/src/main/java/com/danieloliveira/smartfarm/ui/SmartFarmApp.kt
@@ -0,0 +1,12 @@
+package com.danieloliveira.smartfarm.ui
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SmartFarmApp() {
+
+    Surface {
+
+    }
+}
